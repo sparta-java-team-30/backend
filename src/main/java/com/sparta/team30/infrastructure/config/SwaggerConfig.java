1e36fe559c39859b527a67c41dad885d7c38da7c
adlaylatte <dgs2288@naver.com>
2025-02-17 21:16:46 +0900

    chore(configuration): Swagger Configuration (#52)
    - Swagger dependencies 추가
    - Swagger Configuration 구현
    - application.yml swagger 정보 추가

---
+++ src/main/java/com/sparta/team30/infrastructure/config/SwaggerConfig.java
@@ -0,0 +1,31 @@
+package com.sparta.team30.infrastructure.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        Info info = new Info()
+                .title("Team30 API")
+                .version("1.0")
+                .description("Team30 API Docs");
+
+        SecurityScheme securityScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER)
+                .name("Authorization");
+
+        return new OpenAPI()
+                .components(new Components().addSecuritySchemes("bearerAuth", securityScheme))
+                .info(info);
+    }
+}
