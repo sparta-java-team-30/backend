c68e063fa46f0ac59e009fdc443516518d146b4e
jcowwk <jcowwk@gmail.com>
2025-02-18 12:38:06 +0900

    feat(product)/product_create: 상품 조회, 수정, 삭제 기능 구현 (#63)

--- src/main/java/com/sparta/team30/products/controller/ProductController.java
+++ src/main/java/com/sparta/team30/products/controller/ProductController.java
@@ -6,9 +6,10 @@ import com.sparta.team30.products.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
 
 @Controller
 @RequestMapping("/api/products")
@@ -20,5 +21,47 @@ public class ProductController {
     public ResponseEntity<Product> createProduct(@RequestBody ProductDto productDto) {
         Product createdProduct = productService.createProduct(productDto);
         return ResponseEntity.ok(createdProduct);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Product>> getAllProducts() {
+        List<Product> products = productService.getAllProducts();
+        return ResponseEntity.ok(products);
+    }
+
+    @GetMapping("/{productId}")
+    public ResponseEntity<Product> getProductById(@PathVariable UUID productId) {
+        Product product = productService.getProductById(productId);
+        if (product != null) {
+            return ResponseEntity.ok(product);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PatchMapping("/{productId}")
+    public ResponseEntity<Product> updateProduct(@PathVariable UUID productId, @RequestBody ProductDto productDto) {
+        Product updatedProduct = productService.updateProduct(productId, productDto);
+        if (updatedProduct != null) {
+            return ResponseEntity.ok(updatedProduct);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Void> deleteAllProducts() {
+        productService.deleteAllProducts();
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/{productId}")
+    public ResponseEntity<Void> deleteProduct(@PathVariable UUID productId) {
+        boolean isDeleted = productService.deleteProduct(productId);
+        if (isDeleted) {
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
