bd2c07f39591af396946f10c10652c60f3eef01a
jcowwk <jcowwk@gmail.com>
2025-02-19 20:17:59 +0900

    Refactor: 상품 기능 수정 (#95)

---
+++ src/main/java/com/sparta/team30/products/dto/ProductRequestDto.java
@@ -0,0 +1,19 @@
+package com.sparta.team30.products.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+import lombok.Setter;
+import java.util.UUID;
+
+@Getter
+@Setter
+public class ProductRequestDto {
+    @Schema(description = "상품 이름", example = "초콜릿 케이크")
+    private String productName;
+
+    @Schema(description = "상품 가격", example = "12000")
+    private int price;
+
+    @Schema(description = "가게 아이디", example = "3fa85f64-5717-4562-b3fc-2c963f66afa6")
+    private UUID storeId;
+}
--- src/main/java/com/sparta/team30/products/repository/ProductRepository.java
+++ src/main/java/com/sparta/team30/products/repository/ProductRepository.java
@@ -3,7 +3,6 @@ package com.sparta.team30.products.repository;
 import com.sparta.team30.products.domain.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import java.util.UUID;
 
 @Repository
