cabb0f600575e831326800a3169c3de2e4c2fef1
jcowwk <jcowwk@gmail.com>
2025-02-21 11:57:49 +0900

    Refactor: 장바구니 userId 적용 및 response 값 수정 (#101)

--- src/main/java/com/sparta/team30/carts/dto/CartResponseDto.java
+++ src/main/java/com/sparta/team30/carts/dto/CartResponseDto.java
@@ -5,11 +5,13 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.List;
+import java.util.UUID;
 
 @Getter
 @Setter
 @NoArgsConstructor
 public class CartResponseDto {
+    private UUID cartId;
     private Long userId;
     private List<CartItemResponseDto> cartItems;
 }
